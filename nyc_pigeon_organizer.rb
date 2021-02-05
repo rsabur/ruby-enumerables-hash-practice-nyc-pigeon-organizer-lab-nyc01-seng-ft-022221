2c78aa6337776348dacf5abbff42f61e44be9f8a
Radiyah Abdus-Sabur <rsabur58@gmail.com>
2021-02-05 01:50:48 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -4,8 +4,9 @@ def nyc_pigeon_organizer(data)
   # write your code here!
   data.each_with_object({}) do |(key, value), final_array|
     value.each do |inner_key, names|
-      binding.pry
+
       name.each do |name|
+        binding.pry
       end
 
     end

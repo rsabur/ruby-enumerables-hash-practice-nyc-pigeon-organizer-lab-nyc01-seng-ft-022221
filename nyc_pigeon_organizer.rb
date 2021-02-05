5db18eb18d903a21c649ea1e88d5dc6e8dd704fb
Radiyah Abdus-Sabur <rsabur58@gmail.com>
2021-02-05 02:11:48 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -13,7 +13,7 @@ def nyc_pigeon_organizer(data)
           !final_array[name][key] = []
           #binding.pry
         end
-        final_array[name][key].push(inner_key)
+        final_array[name][key].push(inner_key.to_s)
       end
     end
   end

c4e3239e87982abe75d822401a1c56b0682838ef
Radiyah Abdus-Sabur <rsabur58@gmail.com>
2021-02-05 01:36:57 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -3,8 +3,8 @@ require 'pry'
 def nyc_pigeon_organizer(data)
   # write your code here!
 
-data.each_with_object({}) do |(key, value), data|
- data[key] =
+data.each_with_object({}) do |(key, value), final_array|
+ final_array[key] =
  binding.pry
 end
 end
